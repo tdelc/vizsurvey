d198d1d75329594528a9ee1bcda2cee3f1be9d4f
Thomas <t.delclite@gmail.com>
2025-11-05 15:38:43 +0100

    test posit

--- inst/shiny-examples/complete/app.R
+++ inst/shiny-examples/complete/app.R
@@ -1,4 +1,10 @@
 source("ui.R")
 source("server.R")
 
+source("https://raw.githubusercontent.com/tdelc/vizsurvey/master/R/data.R")
+source("https://raw.githubusercontent.com/tdelc/vizsurvey/master/R/prepa.R")
+source("https://raw.githubusercontent.com/tdelc/vizsurvey/master/R/ranking.R")
+source("https://raw.githubusercontent.com/tdelc/vizsurvey/master/R/utils.R")
+source("https://raw.githubusercontent.com/tdelc/vizsurvey/master/R/visu.R")
+
 shinyApp(ui = ui, server = server)
