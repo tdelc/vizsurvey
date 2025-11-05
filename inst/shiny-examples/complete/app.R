2bfd62e39671c63520ddf239da16627c438fb1cc
Thomas <t.delclite@gmail.com>
2025-11-05 15:42:48 +0100

    test posit / add R files

--- inst/shiny-examples/complete/app.R
+++ inst/shiny-examples/complete/app.R
@@ -1,10 +1,10 @@
 source("ui.R")
 source("server.R")
 
-source("https://raw.githubusercontent.com/tdelc/vizsurvey/master/R/data.R")
-source("https://raw.githubusercontent.com/tdelc/vizsurvey/master/R/prepa.R")
-source("https://raw.githubusercontent.com/tdelc/vizsurvey/master/R/ranking.R")
-source("https://raw.githubusercontent.com/tdelc/vizsurvey/master/R/utils.R")
-source("https://raw.githubusercontent.com/tdelc/vizsurvey/master/R/visu.R")
+source("data.R")
+source("prepa.R")
+source("ranking.R")
+source("utils.R")
+source("visu.R")
 
 shinyApp(ui = ui, server = server)
---
+++ inst/shiny-examples/complete/ranking.R
@@ -0,0 +1,28 @@
+#' calculate isoforest score from df
+#'
+#' @param df database
+#'
+#' @returns vector
+#' @export
+#'
+#' @examples
+#' score_isoforest(iris[sapply(iris, is.numeric)])
+score_isoforest <- function(df) {
+
+  rownames_df <- rownames(df)
+  df_scaled <- df %>%
+    ungroup() %>%
+    lapply(scale) %>%
+    as.data.frame()
+
+  if (nrow(df_scaled) < 5) {
+    return(NA)
+  }
+
+  set.seed(123)
+  iso_model <- isotree::isolation.forest(df_scaled, ntrees = 1000, ndim = 1)
+  scores <- predict(iso_model, newdata = df_scaled, type = "score")
+
+  names(scores) <- rownames_df
+  return(scores)
+}
