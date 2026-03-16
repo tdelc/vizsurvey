a4b1478d1f5ff85d90998682e3c8eefedac125b7
Thomas <t.delclite@gmail.com>
2026-03-16 21:52:46 +0100

    Update app.R

--- inst/shiny-examples/app.R
+++ inst/shiny-examples/app.R
@@ -1,14 +1,14 @@
 library(devtools)
 load_all()
-runVizsurvey_from_folder("../extdata",depth_folder = 3)
+# runVizsurvey_from_folder("../extdata",depth_folder = 3)
 
-# link <- "inst/extdata"
-# data_rds_pattern = "global"
-# depth_folder = 3
-# appDir <- system.file("shiny-examples", "complete", package = "vizsurvey")
-# 
-# shiny::shinyOptions(link_data_folder = normalizePath(link))
-# shiny::shinyOptions(data_rds_pattern = data_rds_pattern)
-# shiny::shinyOptions(depth_folder = depth_folder)
-# shiny::runApp(appDir, display.mode = "normal")
+link <- "inst/extdata"
+data_rds_pattern = "global"
+depth_folder = 3
+appDir <- system.file("shiny-examples", "complete", package = "vizsurvey")
+
+shiny::shinyOptions(link_data_folder = normalizePath(link))
+shiny::shinyOptions(data_rds_pattern = data_rds_pattern)
+shiny::shinyOptions(depth_folder = depth_folder)
+shiny::runApp(appDir, display.mode = "normal")
   
